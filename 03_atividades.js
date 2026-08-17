e021531ef962a84b1ed426eb4d9e20e376d83b3b
Idss <ingrid.ssilva5028@gmail.com>
2026-08-17 17:08:33 -0300

    Resolução da atividade

---
+++ 03_atividades.js
@@ -0,0 +1,86 @@
+/*
+//Atividade_0
+console.log("Triângulo " +_______)
+console.log("Triangulo ") +|+ (" Quadro um em baixo do outro")
+*/
+
+/*
+//Atividade_1
+let cidade = "São Carlo "
+let estado = "São Paulo "
+let nome = "Ingrid" 
+
+console.log("Eu " +nome+ "moro em " +cidade+ "no estado de" +estado) 
+*/
+/*
+//Atividade_2 
+let numero1 = 20
+let numero_2 = 5
+let soma = numero1 + numero_2
+
+console.log("A soma do " +numero1+ " com " +numero_2+ " é " +soma)
+*/
+
+/*
+//Atividade_3
+let idade = 25
+let idade_1 = 35
+
+console.log("Eu tenho " +idade+ " só que daqui 10anos vou ter " + idade_1)
+*/
+
+/*
+//Atividade_4
+let numero = 20 
+let numero_dobro = 40
+let numero_triplo = 60 
+
+console.log("O número é " +numero+ " o dobro dele" +numero_dobro+ " e o triplo " +numero_triplo)
+*/
+
+/*
+//Atividade_5
+let nome = "Tv "
+let preco = 1200
+let soma = preco + preco_1 
+let nome_1 = "Videogame "
+let preco_1 = 3000
+
+console.log("A soma é " +preco+ " mas o produto " +preco_1+ "valor" +soma )
+ */
+/*
+//Atividade_6
+let salario = 2500
+let aumento = 500
+let desconto = 250 
+let soma = salario + aumento + desconto 
+
+console.log("O salário é " +salario+ " o aumento " +aumento+ " desconto INSS " + desconto+ " a soma total " +soma )
+*/
+
+/*
+//Atividade_7
+let mesada = 100
+let gasto = 35 
+let sobra = 65
+let soma = mesada - gasto 
+
+console.log("O valor da mesada " +mesada+ " valor do gasto " +gasto+ " soma final " +soma )
+*/ 
+
+/*
+//Atividade_8
+let base = 7
+let altura = 10 
+let soma = base * altura 
+
+console.log("Retângulo com " +base+ " comprimento " +altura+ " soma " +soma) 
+*/ 
+
+//Atividade_9
+let celsius = 30
+let fahrenheit = 320
+let kelvin = 373
+let soma = fahrenheit = celsius * 1.8 + kelvin
+
+console.log("O calculo é " +fahrenheit+ " com o " +celsius+ " ")
