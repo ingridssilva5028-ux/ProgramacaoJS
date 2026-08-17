0d69fff0b1f01e55d10dc5464c4cd145160673f8
Idss <ingrid.ssilva5028@gmail.com>
2026-08-17 14:47:54 -0300

    Revisão e acumulo de variáveis

--- 02_operacoes.js
+++ 02_operacoes.js
@@ -3,14 +3,14 @@ let numero1 = 12
 let numero2 = 8
 let soma = numero1 + numero2
 let subtracao = numero1 - numero2 
-let multiplicacao = nuemro1 * numero2 
+let multiplicacao = numero1 * numero2 
 let divisao = numero1 / numero2 
 let media = (numero1 + numero2) / 2 
 
-console.log("a soma de "+numero1+"com"+numero2 +"é:"+soma) 
-console.log("a subtração de "+numero1+"com"+numero2 +"é:"+subtracao) 
-console.log("a multiplicação de "+numero1+"com"+numero2 +"é:"+multiplicacao) 
-console.log("a divisão de "+numero1+"com"+numero2 +"é:"+divisao)
-console.log("a divisão de "+numero1+"com"+numero2 +"é:"+media) 
+console.log("a soma de " +numero1+ " com " +numero2 +" é: " +soma) 
+console.log("a subtração de " +numero1+ " com " +numero2 +" é: "+subtracao) 
+console.log("a multiplicação de " +numero1+ " com " +numero2 + " é: " +multiplicacao) 
+console.log("a divisão de " +numero1+ " com "+numero2 + " é: " +divisao)
+console.log("a media de " +numero1+ " com " +numero2 + " é: " +media) 
 
 
---
+++ 03_revisao.js
@@ -0,0 +1,32 @@
+/*
+let aluguel = 1200.00 
+let condominio = 250.59
+let internet = 85.99
+let total = aluguel + condominio + internet 
+
+console.log ("o total das contas é: " + total)
+*/ 
+// -------------------------------------------------------------------------
+
+let nome = "Ingrid "
+let sobrenome = "Santos "
+let nome_completo = nome + ""+ sobrenome 
+console.log ( "Seja bem vindo (a) " +nome_completo) 
+ 
+//-------------------------------------------------------------------------
+
+let salario = 2000
+console.log("Seu salário é R$ " +salario)
+
+let comissao = 100 
+salario = salario + comissao // acumulo: adicionar um novo valor na variável 
+console.log("Salário com comissão " + salario) 
+
+let adicional_noturno = 200 
+salario= salario + adicional_noturno
+console.log("Salário com adicional noturno: " + salario) 
+
+/*
+isso é um comentário 
+de multiplas linhas a vontade 
+*/ 
