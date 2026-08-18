402b520460827e274d8b6f271aa9879f2a319686
Idss <ingrid.ssilva5028@gmail.com>
2026-08-18 14:21:29 -0300

    Substituição

--- 03_atividades.js
+++ 03_atividades.js
@@ -1,7 +1,11 @@
 /*
 //Atividade_0
-console.log("Triângulo " +_______)
-console.log("Triangulo ") +|+ (" Quadro um em baixo do outro")
+console.log( ________)
+console.log( |      |) 
+console.log( |      |) 
+console.log( |      |) 
+console.log( |      |) 
+console.log( _______ ) 
 */
 
 /*
@@ -25,9 +29,10 @@ console.log("A soma do " +numero1+ " com " +numero_2+ " é " +soma)
 //Atividade_3
 let idade = 25
 let idade_1 = 35
+let soma = idade + idade_1
 
 console.log("Eu tenho " +idade+ " só que daqui 10anos vou ter " + idade_1)
-*/
+*/ 
 
 /*
 //Atividade_4
@@ -36,9 +41,9 @@ let numero_dobro = 40
 let numero_triplo = 60 
 
 console.log("O número é " +numero+ " o dobro dele" +numero_dobro+ " e o triplo " +numero_triplo)
-*/
+*/ 
 
-/*
+
 //Atividade_5
 let nome = "Tv "
 let preco = 1200
@@ -46,14 +51,14 @@ let soma = preco + preco_1
 let nome_1 = "Videogame "
 let preco_1 = 3000
 
-console.log("A soma é " +preco+ " mas o produto " +preco_1+ "valor" +soma )
- */
+console.log("O produto é " +nome+ " mas o produto " +preco_1+ "valor" +soma )
+ 
 /*
 //Atividade_6
 let salario = 2500
 let aumento = 500
 let desconto = 250 
-let soma = salario + aumento + desconto 
+let soma = salario + aumento - desconto 
 
 console.log("O salário é " +salario+ " o aumento " +aumento+ " desconto INSS " + desconto+ " a soma total " +soma )
 */
@@ -76,7 +81,7 @@ let soma = base * altura
 
 console.log("Retângulo com " +base+ " comprimento " +altura+ " soma " +soma) 
 */ 
-
+/*
 //Atividade_9
 let celsius = 30
 let fahrenheit = 320
@@ -84,3 +89,34 @@ let kelvin = 373
 let soma = fahrenheit = celsius * 1.8 + kelvin
 
 console.log("O calculo é " +fahrenheit+ " com o " +celsius+ " ")
+*/
+/*
+// Atividade_10
+let horas = 2
+let minutos = 2 * 60 
+let segundos = horas * 3600 
+
+console.log("São " +horas+ " Minutos " +minutos+ " segundos " +segundos+ " horário final " +soma)
+*/
+
+/*
+//Atividade_11
+let numero =  13
+let letra = "Groselha "
+let soma = numero + letra 
+let auxiliar = numero 
+let auxiliar1 = letra 
+numero = letra 
+letra = numero 
+console.log("Dentro dessa clase tem " +numero+ " nessa tem " +letra)
+*/
+
+/*
+//Atividade_12 
+let quilometros = 20
+let distância = 12 
+let litros = 100
+let soma = distância + quilometro * litros 
+
+console.log("A distâcia da viagem é " +distância+ " quilometros ," +quilometros+ " litros da gasolina " +litros+ +soma)
+*/
